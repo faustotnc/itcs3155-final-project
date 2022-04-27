30a179b705fd24588cc65527c9577012e25d97dd
HannahSRaju <777hannahraju@gmail.com>
2022-04-27 12:29:11 -0400

    Edited Resources Page

--- App/main.py
+++ App/main.py
@@ -287,8 +287,10 @@ def show_footer_section():
     c1.write("---")
     c1.subheader("UniStats Dashboard")
     c1.write("Empowering Equality in Higher Education.")
-    add_space(24, col=c1)
-    c1.write("The data used in this project is publicly available at this URL: https://www.kaggle.com/datasets/tjkyner/bachelor-degree-majors-by-age-sex-and-state")
+    add_space(25, col=c1)
+    c1.write("The data used in this project is publicly available at this URL:")
+    c1.write("https://www.kaggle.com/datasets/tjkyner/bachelor-degree-majors-by-age-sex-and-state")
+    add_space(25, col=c1)
     c1.write("Visit https://university.graduateshotline.com/ubystate.html to access the contact information of universities in the U.S. by state.")
 
 
